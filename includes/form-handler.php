8fc0eaee97e9ef9453e6db621cd1e568a18e6a54
Oliver Oehme <oliver.oehme@gmail.com>
2024-10-12 13:40:03 +0200

    Update form-handler.php

--- includes/form-handler.php
+++ includes/form-handler.php
@@ -32,8 +32,32 @@ add_shortcode('supporter_form', 'supporter_form_shortcode');
 
 function process_supporter_form() {
     if (isset($_POST['submit_supporter'])) {
-        // Formularverarbeitung und Speichern in der Datenbank
-        // Code wie im vorherigen Beispiel
+        $name = sanitize_text_field($_POST['name']);
+        $email = sanitize_email($_POST['email']);
+        $phone = sanitize_text_field($_POST['phone']);
+        $occupation = sanitize_text_field($_POST['occupation']);
+        $region = sanitize_text_field($_POST['region']);
+        $message = sanitize_textarea_field($_POST['message']);
+        $agreement = isset($_POST['agreement']) ? 1 : 0;
+
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'supporters';
+        $wpdb->insert(
+            $table_name,
+            array(
+                'name' => $name,
+                'email' => $email,
+                'phone' => $phone,
+                'occupation' => $occupation,
+                'region' => $region,
+                'message' => $message,
+                'agreement' => $agreement,
+                'status' => 0 // Status 0 bedeutet, dass der Beitrag auf Freigabe wartet
+            )
+        );
+
+        // Bestätigungsmail an den Unterstützer senden
+        wp_mail($email, 'Bitte bestätigen Sie Ihre Unterstützung', 'Klicken Sie auf den folgenden Link, um Ihre Unterstützung zu bestätigen.');
     }
 }
 add_action('init', 'process_supporter_form');
